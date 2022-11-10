881bd8b85308ac20bfa764ee72e81c7d0b1db425
cwillsey06 <scrawlmin@gmail.com>
2022-11-10 14:07:53 -0700

    Hopefully fix workflow

--- public/build.sh
+++ public/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-npx tsc
-npx stylus src/*.styl --out public/
-npx postcss public/*.css --use autoprefixer -d public/
+function main() {
+    # install dependencies
+    npm i postcss-cli autoprefixer stylus typescript
+
+    # build
+    npx tsc
+    npx stylus src/*.styl --out public/
+    npx postcss public/*.css --use autoprefixer -d public/
+}
+
+time main
